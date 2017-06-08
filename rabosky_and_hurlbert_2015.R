0cf6cfa40468a9dd78273862c7ab8f7ee5fc69c4
richelbilderbeek <richel@richelbilderbeek.nl>
2017-06-08 09:02:38 +0200

    Improved pic

--- rabosky_and_hurlbert_2015.R
+++ rabosky_and_hurlbert_2015.R
@@ -3,9 +3,13 @@ mu <- 0.1
 lambda <- 10.0 * mu # As Liow et al
 k <- 20
 
-crown_ages <- seq(from = 1.0, to = 50, by = 1.0)
-n_crown_ages <- length(crown_ages)
+average_taxon_duration <- 1.0 / mu
+t_end <- average_taxon_duration * 10.0
+
+n_crown_ages <- 100
 n_trees_per_crown_age <- 20
+
+crown_ages <- seq(from = 0.0, to = t_end, length.out = n_crown_ages)
 
 df <- data.frame(
   crown_age = rep(crown_ages, each = n_trees_per_crown_age),
@@ -22,4 +26,7 @@ for (row in seq(1, nrow(df)))
 ggplot2::ggplot(
   data = df,
   ggplot2::aes(x = crown_age, y = gamma)
-) + ggplot2::geom_point() + ggplot2::geom_smooth()
+) +
+  ggplot2::geom_point() +
+  ggplot2::geom_smooth() +
+  ggplot2::geom_vline(xintercept = average_taxon_duration)
