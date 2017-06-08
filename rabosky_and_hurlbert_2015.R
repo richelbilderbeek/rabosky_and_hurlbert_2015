ae7bf35829c88f93b8e4b88d9ef50a6d15b5271a
richelbilderbeek <richel@richelbilderbeek.nl>
2017-06-08 08:32:55 +0200

    Cleaned up

--- rabosky_and_hurlbert_2015.R
+++ rabosky_and_hurlbert_2015.R
@@ -1,17 +1,15 @@
-set.seed(43)
-crown_age <- 10
+set.seed(42)
 mu <- 0.1
 lambda <- 10.0 * mu # As Liow et al
 k <- 20
 
-# Crown ages
-ts <- seq(1,50,1.0)
-
+crown_ages <- seq(from = 1.0, to = 50, by = 1.0)
+n_crown_ages <- length(crown_ages)
 n_trees_per_crown_age <- 20
 
 df <- data.frame(
-  crown_age = rep(ts, each = n_trees_per_crown_age),
-  gamma = rep(NA, length(ts) * n_trees_per_crown_age)
+  crown_age = rep(crown_ages, each = n_trees_per_crown_age),
+  gamma = rep(NA, n_crown_ages * n_trees_per_crown_age)
 )
 
 for (row in seq(1, nrow(df)))
